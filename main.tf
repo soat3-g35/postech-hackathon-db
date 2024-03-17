a4305b0e058d93d645400aaa91f6b61a51aea98e
aline.tanure <alinedonatoc@gmail.com>
2024-03-17 14:44:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,15 +70,15 @@ resource "aws_db_subnet_group" "education" {
 }
 
 resource "aws_db_instance" "education" {
-  identifier          = "education"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 5
-  engine              = "postgres"
-  engine_version      = "14.11"
-  username            = "postgres"
-  password            = "postgres"
-  publicly_accessible = true
-  skip_final_snapshot = true
+  identifier             = "education"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.11"
+  username               = "postgres"
+  password               = "postgres"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.education.name
   vpc_security_group_ids = [aws_security_group.instance.id]
 
