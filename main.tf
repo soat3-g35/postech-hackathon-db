48177c59173ea30858f600b834658f51026bebba
aline.tanure <alinedonatoc@gmail.com>
2024-03-17 11:14:12 -0300

    Add tags

--- main.tf
+++ main.tf
@@ -3,8 +3,23 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["postech-vpc"]
+  }
+}
+
+data "aws_subnet" "selected" {
+  filter {
+    name   = "tag:kubernetes.io/cluster/education-eks-cluster"
+    values = ["shared"]
+  }
+}
+
 resource "aws_security_group" "instance" {
-  name = "postgres-security-group"
+  name   = "postgres-security-group"
+  vpc_id = data.aws_vpc.selected.id
   ingress {
     from_port   = 5432
     to_port     = 5432
@@ -14,16 +29,19 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_db_instance" "education" {
-  identifier             = "education"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "14.11"
-  username = "postgres"
-  password = "postgres"
-  publicly_accessible    = true
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  identifier          = "education"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 5
+  engine              = "postgres"
+  engine_version      = "14.11"
+  username            = "postgres"
+  password            = "postgres"
+  publicly_accessible = true
+  skip_final_snapshot = true
+
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id              = data.aws_subnet.selected.id
+
   db_name = "mypostgres"
 
   tags = {
