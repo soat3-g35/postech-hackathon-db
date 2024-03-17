67098037c1cdd1733f20d99bbf4b6b1f77793d98
aline.tanure <alinedonatoc@gmail.com>
2024-03-17 10:53:51 -0300

    Change db instructions

--- main.tf
+++ main.tf
@@ -3,60 +3,13 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
-
-  name                 = "education"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-
-resource "aws_db_subnet_group" "education" {
-  name       = "education"
-  subnet_ids = module.vpc.public_subnets
-
-  tags = {
-    Name = "Education"
-  }
-}
-
-resource "aws_security_group" "rds" {
-  name   = "education_rds"
-  vpc_id = module.vpc.vpc_id
-
+resource "aws_security_group" "instance" {
+  name = "postgres-security-group"
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "education_rds"
-  }
-}
-
-resource "aws_db_parameter_group" "education" {
-  name   = "education"
-  family = "postgres14"
-
-  parameter {
-    name  = "log_connections"
-    value = "1"
   }
 }
 
@@ -66,11 +19,14 @@ resource "aws_db_instance" "education" {
   allocated_storage      = 5
   engine                 = "postgres"
   engine_version         = "14.11"
-  username               = "education"
-  password               = "education"
-  db_subnet_group_name   = aws_db_subnet_group.education.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  parameter_group_name   = aws_db_parameter_group.education.name
+  username = "postgres"
+  password = "postgres"
   publicly_accessible    = true
   skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  db_name = "mypostgres"
+
+  tags = {
+    Name = "MyPostgresDB"
+  }
 }
