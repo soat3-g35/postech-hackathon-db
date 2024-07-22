6709e44e5075be8ea0a158835e08a59353fbec40
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-07-22 07:45:15 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,18 +3,18 @@
 
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.pedido.address
+  value       = aws_db_instance.medico.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.pedido.port
+  value       = aws_db_instance.medico.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.pedido.username
+  value       = aws_db_instance.medico.username
   sensitive   = true
 }
